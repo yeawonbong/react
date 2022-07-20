f9b64314fccb18cf893b99f5002036a1346c35ca
yeawonbong <ybong.develop@gmail.com>
2022-07-21 08:54:20 +0900

    0721

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,23 +1,61 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+// import styled from "styled-components";
+
+// let YellowBtn = styled.button`
+//   background : ${ props => props.bg };
+//   color : ${ props => props.bg == 'blue' ? 'white' : 'black' };
+//   padding : 10px; 
+// `
 
 const Detail = (props) => {
-  
-  let {id} = useParams();
-  const imgsrc = `https://codingapple1.github.io/shop/shoes${id}.jpg`
+
+  const [alert, setAlert] = useState(true)
+  const [text, setText] = useState(true)
+  let { id } = useParams();
+  const imgsrc = `https://codingapple1.github.io/shop/shoes${Number(id) + 1}.jpg`
+  let 찾은상품 = props.shoes.find(function(x){
+    return x.id == id
+  });
+
+  useEffect(() => {
+    let timer = setTimeout(() => { setAlert(false) }, 2000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isNaN(Number(text))){
+      window.alert("숫자를 입력해주세요")
+    }
+    else
+      console.log(Number(text))
+
+  }, [text])
 
 	return (
 	  <div className="container">
-		<div className="row">
-		  <div className="col-md-6">
-			<img src={imgsrc} alt="shoe" width="100%" />
-		  </div>
-		  <div className="col-md-6 mt-4">
-			<h4 className="pt-5">{props.shoes[id].title}</h4>
-			<p>{props.shoes[id].content}</p>
-			<p>{props.shoes[id].price}</p>
-			<button className="btn btn-danger">주문하기</button> 
-		  </div>
-		</div>
+      {
+        alert == true ?
+          <div className="alert alert-warning">
+            2초이내 구매시 할인
+          </div>
+        : null
+      }
+      <input onChange={ (e) => {setText(e.nativeEvent.data); console.log(e.nativeEvent.data)} }></input>
+      <div className="row">
+        <div className="col-md-6">
+        <img src={imgsrc} alt="shoe" width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+        <h4 className="pt-5">{찾은상품.title}</h4>
+        <p>{찾은상품.content}</p>
+        <p>{찾은상품.price}</p>
+        <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
 	  </div>  
 	)
   }
--- shop/src/routes/Main.js
+++ shop/src/routes/Main.js
@@ -1,33 +1,61 @@
 import { Col, Container, Row } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 import bg from '../img/bg.png'
+import axios from 'axios'
+import { useState } from 'react'
 
 const Card = (props) => {
-	return (
-	<Col sm>
-	<img src={`https://codingapple1.github.io/shop/shoes${props.idx + 1}.jpg`} alt={`shoes${props.idx + 1}`} width="80%"/>
-	<h4>{props.shoes.title}</h4>
-	<p>{props.shoes.price}</p>
-	</Col>
-	)
-  }
+  let navigate = useNavigate()
+
+  return (
+    <Col sm>
+      <img src={`https://codingapple1.github.io/shop/shoes${props.idx + 1}.jpg`} alt={`shoes${props.idx + 1}`} onClick={() => navigate(`/Detail/${props.idx}`)} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </Col>
+  )
+}
 
 const Main = (props) => {
-	return (
-	  <div>
-	  <div className='main-bg' style={{ backgroundImage : `url(${bg})` }}></div>
-	  <Container>
-		<Row>
-		  {
-			props.shoes.map((shoe, i) => {
-			  return <Card shoes={shoe} idx={i}/>
-			})
-		  }
-		</Row>
-	  </Container>
-	  </div>
-	)
-  }
+  let [shoes, setShoes] = useState([...props.shoes])
+  let [cnt, setCnt] = useState(2)
+  // console.log(props.shoes)
+
+  return (
+    <div>
+      <div className='main-bg' style={{ backgroundImage: `url(${bg})` }}></div>
+      <Container>
+        <Row>
+          {
+            shoes.map((shoe, i) => {
+              return (
+                <Col sm='4'>
+                  <Card shoes={shoe} idx={i} key={i} />
+                </Col>
+              )
+            })
+          }
+        </Row>
+      </Container>
+      {
+        cnt < 4 ? 
+        <button onClick={() => {
+          setCnt(cnt + 1)
+          console.log(cnt)
+          axios.get(`https://codingapple1.github.io/shop/data${cnt}.json`)
+            .then((data) => {
+              setShoes([...shoes, ...data.data])
+              console.log(shoes)
+            })
+            .catch(() => {
+              console.log('실패')
+            })
+        }}>더보기</button>
+        : null
+      }
+    </div>
+  )
+}
 
 export default Main
-  
-  
+
